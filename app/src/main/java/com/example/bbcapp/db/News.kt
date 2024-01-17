21db353e1953140fe41614b1d942b6c716823834
gabriel-hoa <gabrielhoaraujo100@gmail.com>
2024-01-17 11:33:31 -0300

    db articles, dataroom and typeconverter

--- app/src/main/java/com/example/bbcapp/db/News.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bbcapp.db
-
-data class News(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/example/bbcapp/db/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bbcapp.db
-
-data class Source(
-    val id: String,
-    val name: String
-)
